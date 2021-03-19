d6ac0cb3c322ef607a6b14795e4e3a787853b09e
Core Dumper <contactprem@gmail.com>
2021-03-19 05:16:09 -0700

    Update to  Python3 and add logging/debug
    Update to  Python3 and add logging/debug options

--- tools/mp3clean.py
+++ tools/mp3clean.py
@@ -1,34 +1,39 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import eyed3
 import sys
 import logging
+import traceback
+import argparse
 logging.getLogger("eyed3").setLevel(logging.ERROR)
 
-def stripStarMusiq(data):
-    if data == None : return None
-    for item in ['5starmusiq','starmusiq','vmusiq','123musiq','musiq','clicktamil']:
-        idx = data.lower().find(item)
-        if idx >= 0: break
-
-    if idx == -1 : return data
-
-    # find the last -
-    hidx = data.rfind('-',0,idx)
+def stripStarMusiq(d):        
+    if d == None : return None
+    idx = len(d)
+    for suffix in ['5starmusiq','starmusiq','starmusiq.fun','starmusiq.top','vmusiq','123musiq','musiq','clicktamil','maango','www', 'Feel The Difference','Original CD']:
+        pos = d.lower().find(suffix.lower())
+        if pos >= 0 :
+            idx = min(idx, pos)
+    logging.debug('{}:{}'.format(idx, d))
+    if idx == -1 : return d
+    # find the last --->
+    hidx = d.rfind('-',0,idx)
     if hidx == -1:
-        hidx = data.rfind('[',0,idx)
+        hidx = d.rfind('[',0,idx)
     idx = hidx if hidx >= 0 else idx
-    
-    if idx == -1 :
-        print 'unable to find hypen in', data
-        return data
 
-    return data[0:idx].strip()
+    if idx == -1 :
+        logging.debug('unable to find hypen in {}'.format(d))
+        return d
+    logging.debug('{} ---> {}'.format(d,d[0:idx].strip()))
+    return d[0:idx].strip()
+        
+        
 
 def processFile(filename):
     modded = False
     if not filename.endswith('.mp3'):
-        print 'file not an mp3 [{}]'.format(filename)
+        logging.error('file not an mp3 [{}]'.format(filename))
         return modded
 
     mp3 = eyed3.load(filename)
@@ -41,18 +46,27 @@ def processFile(filename):
 
 
     # go through the tags directly
-    for k,frames in mp3.tag.frame_set.iteritems():
+    for k,frames in mp3.tag.frame_set.items():
         for n in range(0, len(frames)):
-            try:
-                d = frames[n].__getattribute__('text')
-                stripd = stripStarMusiq(d)
-                #print d!=stripd, k, stripd, d
-                if stripd != d:
-                    frames[n].text = stripd
-                    modded = True
-            except Exception, e:
-                #print e
-                pass
+            for attr in ['text','url']:
+                try:
+                    if attr not in dir(frames[n]):
+                        continue
+                    if attr == 'text':
+                        d = frames[n].text
+                    else:
+                        d = frames[n].url
+                    stripd = stripStarMusiq(d)
+                    logging.debug('{} - {} - {} - {} - {}'.format(d!=stripd, k, stripd, d, type(frames[n])))
+                    if stripd != d:
+                        if attr == 'text':
+                            frames[n]._text = stripd
+                        else:
+                            frames[n].url  = stripd
+                        modded = True
+                except Exception as e:
+                    logging.debug(e)
+                    #traceback.print_exc();
 
     data = stripStarMusiq(mp3.tag.title)
     if data != mp3.tag.title:
@@ -77,14 +91,14 @@ def processFile(filename):
     if mp3.tag.genre:
         data = stripStarMusiq(mp3.tag.genre.name)
         if data != mp3.tag.genre.name:
-            #print mp3.tag.genre.name, data
+            logging.debug(mp3.tag.genre.name, data)
             modded = True
             mp3.tag.genre.name = data
 
     if mp3.tag.non_std_genre:
         data = stripStarMusiq(mp3.tag.non_std_genre.name)
         if data != mp3.tag.non_std_genre.name:
-            #print mp3.tag.non_std_genre.name, data
+            logging.debug(mp3.tag.non_std_genre.name, data)
             modded = True
             mp3.tag.non_std_genre.name = data
 
@@ -96,12 +110,14 @@ def processFile(filename):
                 modded = True
 
     if modded:
+        logging.debug('saving file')
         mp3.tag.save()
 
-    newdata = stripStarMusiq(filename)
-    if newdata != filename:
+    newfilename = stripStarMusiq(filename)
+    if newfilename != filename:
         #rename
-        os.rename(filename, newdata+".mp3")
+        logging.debug('renaming file')
+        os.rename(filename, newfilename+".mp3")
         modded=True
 
     return modded
@@ -114,9 +130,10 @@ def processFiles(files):
             count += 1
             try:
                 ret = processFile(filename)
-                print '{} : {} : {}'.format(count, ret, filename)
-            except:
-                print 'unable to process : {}'.format(filename)
+                logging.info('{} :{}: {}'.format(count, '*' if ret else ' ', filename))
+            except Exception as e:
+                logging.debug(e)
+                logging.warning('unable to process : {}'.format(filename))
 
         elif os.path.isdir(filename):
             for filetuple in os.walk(filename):
@@ -126,11 +143,22 @@ def processFiles(files):
                     fname = '{}/{}'.format(filetuple[0],f)
                     try:
                         ret = processFile(fname)
-                        print '{} : {} : {}'.format(count, ret, fname)
-                    except:
-                        print 'unable to process : {}'.format(fname)
+                        logging.info('{} :{}: {}'.format(count, '*' if ret else ' ', fname))
+                    except Exception as e:
+                        logging.debug(e)
+                        logging.warning('unable to process : {}'.format(fname))
 
 ###### main function ####
 
-#print sys.argv
-processFiles(sys.argv[1:])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='MP3 Cleaner')
+    parser.add_argument('files', nargs='*', default=None)
+    parser.add_argument('-d', '--debug', action=argparse.BooleanOptionalAction, default=False)
+    args = parser.parse_args()
+
+    loglevel = logging.INFO
+    if args.debug:
+        loglevel = logging.DEBUG
+
+    logging.basicConfig(level=loglevel, format="%(levelname)s: %(message)s")
+    processFiles(args.files)
